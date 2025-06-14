72a4eaf412c575c4da764721f8196b795ca91bae
JamshidDev <raximovj662@gmail.com>
2025-06-14 12:26:40 +0500

    added config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './style.css'
 import App from './App.vue'
 import naive from 'naive-ui'
 import router from './router'
+import i18n from "./i18n/index.js"
+import pinia from "./store/index.js"
 
 
 const meta = document.createElement('meta')
@@ -13,4 +15,6 @@ document.head.appendChild(meta)
 const app = createApp(App);
 app.use(naive)
 app.use(router)
+app.use(pinia)
+app.use(i18n)
 app.mount('#app')
