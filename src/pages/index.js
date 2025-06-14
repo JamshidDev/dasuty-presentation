2f43e00d9d8019670f2b17e23bec8d361abb3e1a
marikspectre <uboboqulov00@gmail.com>
2025-06-14 18:14:08 +0500

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export {default as LoginPage} from "./Login/page.vue"
 export {default as HomePage} from "./Home/page.vue"
-export {default as DashboardPage} from "./dashboard/page.vue"
 export {default as GantChartPage} from "./gantChart/page.vue"
+export {default as DashboardPage} from "./Home/page.vue"
 
 export const ProjectsPage = ()=>import("./Projects/ProjectsPage.vue")
 export const ProjectInstancePage = ()=>import("./Projects/ProjectInstancePage.vue")
