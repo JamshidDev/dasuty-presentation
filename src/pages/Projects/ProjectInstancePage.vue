2b7008ee5b81dcf13fb43b6a837299db11af68aa
shakhrillodev <shaxrillonusratov@gmail.com>
2025-06-14 18:34:12 +0500

    added workers page

--- src/pages/Projects/ProjectInstancePage.vue
+++ src/pages/Projects/ProjectInstancePage.vue
@@ -1,134 +1,27 @@
 <script setup>
-import UIPageContent from '@/components/ui/UIPageContent.vue';
-import Document from './ui/Document.vue'
 import { GantChartPage } from "@/pages/index.js"
-
-const sample = {
-    name: "Railmap",
-    description: "Hamma ma'lumotlar bitta xaritada — real vaqtda nazorat va tahlil",
-    percentage: 50
-}
-
-const modules = [sample, sample, sample, sample, sample, sample, sample, sample, sample,]
-
+import ProjectGeneralPage from "../projectGeneral/ProjectGeneralPage.vue"
+import ProjectWorkerPage from "../projectWorker/ProjectWorkerPage.vue"
 </script>
 <template>
-    <n-tabs type="segment" animated >
+    <n-tabs type="segment" animated class="projects_tab">
         <n-tab-pane name="Umumiy ma'lumotlar" tab="Umumiy ma'lumotlar">
-            <UIPageContent>
-
-                <div class="flex flex-col gap-[72px]">
-
-                    <div class="flex gap-10 items-center">
-                        <img src="/imgs/sample.jpg" alt="sample" class="rounded-2xl aspect-[2/1] object-cover w-[50%]">
-                        <div class="grow basis-auto">
-                            <p class="text-4xl font-bold text-text-icon-primary1-mutable">Railmap</p>
-                            <p class="mt-2 text-text-icon-secondary1-mutable font-medium text-lg leading-7">Railmap — bu
-                                O‘zbekiston temir yo‘llarining holatini yagona interaktiv xaritada real
-                                vaqt
-                                rejimida kuzatish va tahlil qilish imkonini beruvchi zamonaviy raqamli boshqaruv
-                                tizimidir.
-                            </p>
-                            <div class="flex mt-8">
-                                <div class="grow basis-auto">
-                                    <p class="text-xs text-text-icon-secondary1-mutable leading-[16px]">Boshlangan sana
-                                    </p>
-                                    <p
-                                        class="mt-[2px] text-brand-background-text-immutable text-lg font-bold leading-[20px]">
-                                        14-iyun, 2025</p>
-                                </div>
-                                <div class="grow basis-auto">
-                                    <p class="text-xs text-text-icon-secondary1-mutable leading-[16px]">Yuklangan
-                                        hujjatlar
-                                    </p>
-                                    <p
-                                        class="mt-[2px] text-brand-background-text-immutable text-lg font-bold leading-[20px]">
-                                        7 ta</p>
-                                </div>
-                                <div class="grow basis-auto">
-                                    <p class="text-xs text-text-icon-secondary1-mutable leading-[16px]">Xodimlar soni
-                                    </p>
-                                    <p
-                                        class="mt-[2px] text-brand-background-text-immutable text-lg font-bold leading-[20px]">
-                                        12 ta</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex flex-col gap-8">
-                        <p class="text-text-icon-primary2-mutable font-bold text-4xl leading-11">Hujjatlar</p>
-                        <div class="grid gap-4 grid-cols-3">
-                            <Document />
-                            <Document />
-                            <Document />
-                            <Document />
-                            <Document />
-                            <Document />
-                            <Document />
-                            <Document :type="0" />
-                        </div>
-                    </div>
-                    <div class="flex flex-col gap-8">
-                        <p class="text-text-icon-primary2-mutable font-bold text-4xl leading-11">Loyiha modul</p>
-                        <div class="grid grid-cols-4 modules_grid">
-                            <template v-for="(module, idx) in modules" :key="idx">
-                                <div class="col-span-4 h-[70px] grid_separator relative" v-if="idx && idx % 4 == 0">
-                                </div>
-                                <div class="modules_grid-item flex flex-col gap-3 border-t-2 border-b-2 border-brand-light-background-mutable-hover p-5 pl-0 relative"
-                                    :class="{ 'row-last': (idx + 1) % 4 == 0 }">
-                                    <p class="font-bold text-lg text-text-icon-primary1-mutable">{{ module.name }}</p>
-                                    <p class="font-medium text-text-icon-secondary1-mutable">{{ module.description }}
-                                    </p>
-                                    <div class="flex gap-5 items-center">
-                                        <p class="w-[20px] font-semibold">{{ module.percentage }}%</p>
-                                        <div class="grow basis-auto">
-                                            <n-progress :height="12" type="line" :percentage="module.percentage"
-                                                class="w-full!" />
-                                        </div>
-                                    </div>
-                                    <p
-                                        class="absolute w-[40px] h-[40px] flex justify-center items-center rounded-xl bg-brand-light-background-mutable-hover text-brand-background-text-immutable font-bold text-lg top-0 translate-y-[-50%]">
-                                        {{ idx + 1 }}</p>
-                                </div>
-                            </template>
-
-                        </div>
-                    </div>
-                    <div class="h-[200px]"></div>
-                </div>
-            </UIPageContent>
-
+            <ProjectGeneralPage />
         </n-tab-pane>
         <n-tab-pane name="Gant chart" tab="Gant chart">
             <GantChartPage />
         </n-tab-pane>
         <n-tab-pane name="Xodimlar" tab="Xodimlar">
-            Xodimlar
+            <ProjectWorkerPage/>
         </n-tab-pane>
     </n-tabs>
 </template>
 
-<style scoped>
-.row-last {
-    border-right: 2px solid var(--color-brand-light-background-mutable-hover);
-    border-top-right-radius: 20px;
-    border-bottom-right-radius: 20px;
+<style>
+.projects_tab .n-tabs-rail{
+    margin-left: auto;
+    margin-right: auto;
+    width: 600px;
 }
 
-.grid_separator::before {
-    content: '';
-    position: absolute;
-    top: -2px;
-    bottom: -2px;
-    left: -30px;
-    width: 30px;
-    border: 2px solid var(--color-brand-light-background-mutable-hover);
-    border-right: none;
-    border-top-left-radius: 20px;
-    border-bottom-left-radius: 20px;
-}
-
-.modules_grid-item:last-child {
-    border: none;
-}
 </style>
---
+++ src/pages/projectGeneral/ui/Document.vue
@@ -0,0 +1,63 @@
+<script setup>
+import {Icons} from "@/shared/icons"
+
+defineProps({
+    type: {
+        default: 1,
+        type: Number
+    }
+})
+</script>
+<template>
+    <div v-if="type == 1" class="rounded-3xl border-[0.4px] p-2 pr-4 bg-bright-mutable">
+        <div class="flex gap-4 items-center">
+            <div
+                class="rounded-2xl bg-brand-light-background-mutable flex justify-center items-center h-[64px] w-[64px]">
+                <img src="/imgs/document.png" class="w-[40px] h-[40px]" />
+            </div>
+            <div class="flex justify-between grow basis-auto items-center">
+                <div>
+                    <p class="font-bold text-lg">Reestr.uz xulosasi</p>
+                    <div
+                        class="px-2 py-1 rounded-lg bg-success-light-background-mutable inline-block border border-black/5">
+                        <p class="text-success-background-immutable inline-block font-semibold text-sm leading-4">
+                            Yuklangan</p>
+                    </div>
+                </div>
+                <n-button class="!w-[44px] !h-[44px]" type="primary">
+                    <template #icon>
+                        <n-icon :size="24">
+                            <Icons.DownloadSolidIcon />
+                        </n-icon>
+                    </template>
+                </n-button>
+            </div>
+        </div>
+    </div>
+    <div v-else class="rounded-3xl border-[0.4px] p-2 pr-4 bg-bright-mutable">
+        <div class="flex gap-4 items-center">
+            <div
+                class="rounded-2xl bg-brand-light-background-mutable flex justify-center items-center h-[64px] w-[64px]">
+                <img src="/imgs/document.png" class="w-[40px] h-[40px]" />
+            </div>
+            <div class="flex justify-between grow basis-auto items-center">
+                <div>
+                    <p class="font-bold text-lg">Dalolatnoma</p>
+                    <div
+                        class="px-2 py-1 rounded-lg bg-danger-light-background-mutable inline-block border border-black/5">
+                        <p class="text-danger-background-immutable inline-block font-semibold text-sm leading-4">
+                            Yuklanmagan
+                        </p>
+                    </div>
+                </div>
+                <n-button secondary class="!w-[44px] !h-[44px]" type="primary">
+                    <template #icon>
+                        <n-icon :size="24" class="text-brand-text-disabled-mutable">
+                            <Icons.DownloadSolidIcon />
+                        </n-icon>
+                    </template>
+                </n-button>
+            </div>
+        </div>
+    </div>
+</template>
