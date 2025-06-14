972169f4970a21b9c076cf37185639d4bf961681
JamshidDev <raximovj662@gmail.com>
2025-06-14 15:35:37 +0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import {customTheme, naiveBreakpoints} from "@/assets/theme"
 import { uzUZ, dateUzUZ, ruRU, dateRuRU, enUS, dateEnUS, }  from 'naive-ui'
 import i18n from "@/i18n/index"
-import ClientLayout from "@/Layout/ClientLayout.vue"
+import {clientApp} from "@/Layout/index.js"
 
 const localProvider = computed(()=>{
   if(i18n.global.locale==='uz'){
@@ -33,7 +33,7 @@ const localProvider = computed(()=>{
   >
     <n-message-provider placement="top-right">
       <n-dialog-provider>
-        <ClientLayout/>
+        <clientApp/>
       </n-dialog-provider>
     </n-message-provider>
   </n-config-provider>
---
+++ src/Layout/index.js
@@ -0,0 +1,2 @@
+export {default as clientApp} from './client/clientApp.vue';
+export {default as emptyApp} from './empty/emptyApp.vue';
