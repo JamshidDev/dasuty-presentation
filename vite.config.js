e4c591a38348ca222904074f7837940115e47769
JamshidDev <raximovj662@gmail.com>
2025-06-14 11:53:35 +0500

    added

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, 'src')
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['media-theme-tailwind-audio', 'media-theme-yt'].includes(tag),
+        }
+      }
+    }),
+  ],
+  server:{
+    port:8000
+  },
+})
