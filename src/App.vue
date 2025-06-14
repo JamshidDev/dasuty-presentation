9bd56f8b43f7b8a2fcab83bcf2511ce9b41a5a56
shakhrillodev <shaxrillonusratov@gmail.com>
2025-06-14 13:22:28 +0500

    update theme

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <script setup>
-
+import {customTheme, naiveBreakpoints} from "@/assets/theme"
+import { uzUZ, dateUzUZ, ruRU, dateRuRU, enUS, dateEnUS, }  from 'naive-ui'
+import i18n from "@/i18n/index"
 import ClientLayout from "@/Layout/ClientLayout.vue"
+
+const localProvider = computed(()=>{
+  if(i18n.global.locale==='uz'){
+    return {
+      lang: uzUZ,
+      date: dateUzUZ,
+    }
+  }else if(i18n.global.locale==='en'){
+    return {
+      lang: enUS,
+      date: dateEnUS,
+    }
+  }else if(i18n.global.locale==='ru'){
+    return {
+      lang: ruRU,
+      date: dateRuRU,
+    }
+  }
+})
 </script>
 
 <template>
-  <n-config-provider>
+  <n-config-provider
+      :breakpoints="naiveBreakpoints"
+      :theme-overrides="customTheme()"
+      :locale="localProvider.lang"
+      :date-locale="localProvider.date"
+  >
     <n-message-provider placement="top-right">
       <n-dialog-provider>
         <ClientLayout/>
